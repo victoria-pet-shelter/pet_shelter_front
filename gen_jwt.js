f8e893f46ca80dfd5901a5978a6ba9955fe9a548
Mark Korobkin <markkorobkin85@gmail.com>
2025-05-14 15:10:26 +0300

    removed redundant file

--- gen_jwt.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-const secretKey = 'petshelter69420';
-
-const token = jwt.sign({
-  id: 1,
-  username: 'pets'
-}, secretKey, { expiresIn: '1h' });
-
-console.log(token);
