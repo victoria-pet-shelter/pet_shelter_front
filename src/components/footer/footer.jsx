2af10c73ded369f96003b63372159caf97aa6d1d
Alan Arzumanjan <alanarzumanjan@gmail.com>
2025-06-14 16:04:38 +0300

    permision delete

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,17 +1,17 @@
-import './footer.css';
-import { useState, useEffect } from 'react';
-import Toggle from "react-toggle";
-import { useMediaQuery } from "react-responsive";
+import "./footer.css";
 import facebookLogo from "../../assets/footer/facebook.png";
 import twitterLogo from "../../assets/footer/twitter.png";
 import vimeoLogo from "../../assets/footer/vimeo.png";
-import useLocalStorage from 'use-local-storage';
+import useLocalStorage from "use-local-storage";
 
 function Footer() {
-  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+  const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [theme, setTheme] = useLocalStorage(
+    "theme",
+    defaultDark ? "dark" : "light"
+  );
   const switchTheme = () => {
-    const newTheme = theme === 'light' ? 'dark' : 'light';
+    const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
   };
 
@@ -19,12 +19,10 @@ function Footer() {
     <div class="footer-1" data-theme={theme}>
       <div class="footer-2">
         <button onClick={switchTheme}>
-          Switch to {theme === 'light' ? 'dark' : 'light'} theme
+          Switch to {theme === "light" ? "dark" : "light"} theme
         </button>
         <a href="/contact">
-          <p class="footer-text">
-            Contacts
-          </p>
+          <p class="footer-text">Contacts</p>
         </a>
         <div class="node-1">
           <a href="https://facebook.com">
@@ -38,22 +36,16 @@ function Footer() {
           </a>
         </div>
         <a href="/about">
-          <p class="footer-text">
-            About us
-          </p>
+          <p class="footer-text">About us</p>
         </a>
       </div>
       <div class="footer-3">
         <div class="sub-footer">
-          <p class="footer-text">
-            Terms of Service
-          </p>
+          <p class="footer-text">Terms of Service</p>
           <p class="footer-text">
             © 2025 Pet Adoption Center. All Rights Reserved.
           </p>
-          <p class="footer-text">
-            Privacy Policy
-          </p>
+          <p class="footer-text">Privacy Policy</p>
         </div>
       </div>
     </div>
