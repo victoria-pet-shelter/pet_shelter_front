52dc5dc67ce169f051fd4a42a380d7595175c869
Alan Arzumanjan <alanarzumanjan@gmail.com>
2025-06-18 04:35:58 +0300

    Small styles changes - add eye eyehide for password in login register and etc

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,6 +2,8 @@ import "./footer.css";
 import facebookLogo from "../../assets/footer/facebook.png";
 import twitterLogo from "../../assets/footer/twitter.png";
 import vimeoLogo from "../../assets/footer/vimeo.png";
+import sunIcon from "../../assets/footer/light.png";
+import moonIcon from "../../assets/footer/dark.svg";
 import useLocalStorage from "use-local-storage";
 
 function Footer() {
@@ -10,42 +12,48 @@ function Footer() {
     "theme",
     defaultDark ? "dark" : "light"
   );
+
   const switchTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
   };
 
   return (
-    <div class="footer-1" data-theme={theme}>
-      <div class="footer-2">
-        <button onClick={switchTheme}>
-          Switch to {theme === "light" ? "dark" : "light"} theme
+    <div className="footer-1" data-theme={theme}>
+      <div className="footer-2">
+        <button onClick={switchTheme} className="theme-button">
+          <img
+            src={theme === "light" ? moonIcon : sunIcon}
+            alt="Toggle Theme"
+            className="theme-icon"
+          />
         </button>
+
         <a href="/contact">
-          <p class="footer-text">Contacts</p>
+          <p className="footer-text">Contacts</p>
         </a>
-        <div class="node-1">
+        <div className="node-1">
           <a href="https://facebook.com">
-            <img src={facebookLogo} class="logo" alt="Facebook" />
+            <img src={facebookLogo} className="logo" alt="Facebook" />
           </a>
           <a href="https://x.com">
-            <img src={twitterLogo} class="logo" alt="Twitter" />
+            <img src={twitterLogo} className="logo" alt="Twitter" />
           </a>
           <a href="https://vimeo.com">
-            <img src={vimeoLogo} class="logo" alt="Vimeo" />
+            <img src={vimeoLogo} className="logo" alt="Vimeo" />
           </a>
         </div>
         <a href="/about">
-          <p class="footer-text">About us</p>
+          <p className="footer-text">About us</p>
         </a>
       </div>
-      <div class="footer-3">
-        <div class="sub-footer">
-          <p class="footer-text">Terms of Service</p>
-          <p class="footer-text">
+      <div className="footer-3">
+        <div className="sub-footer">
+          <p className="footer-text">Terms of Service</p>
+          <p className="footer-text">
             © 2025 Pet Adoption Center. All Rights Reserved.
           </p>
-          <p class="footer-text">Privacy Policy</p>
+          <p className="footer-text">Privacy Policy</p>
         </div>
       </div>
     </div>
